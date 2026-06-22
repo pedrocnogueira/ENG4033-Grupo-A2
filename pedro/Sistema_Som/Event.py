e2465cc7b23df703e7b8ebb08680e9861d520d52
pedro.nogueira <pedro.crnogueira@gmail.com>
2026-06-22 14:54:48 -0300

    refactor: simplify Event class by removing redundant type declaration

--- pedro/Sistema_Som/Event.py
+++ pedro/Sistema_Som/Event.py
@@ -3,8 +3,8 @@ from dataclasses import dataclass
 
 @dataclass
 class Event:
+    type: str = "note_on"
     note: int
     duration: int
     channel: int = 0
     velocity: int = 0
-    type: str = "note_on"
